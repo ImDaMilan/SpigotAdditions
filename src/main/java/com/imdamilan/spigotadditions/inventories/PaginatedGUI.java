eb4a09da4295c3cf68d4048af0fc6fa9b4c9c406
ImDaMilan <imdamilan333@gmail.com>
2022-10-27 15:29:10 +0200

    Added more methods to PaginatedGUI

--- src/main/java/com/imdamilan/spigotadditions/inventories/PaginatedGUI.java
+++ src/main/java/com/imdamilan/spigotadditions/inventories/PaginatedGUI.java
@@ -26,6 +26,10 @@ public class PaginatedGUI {
         this.pages = new ArrayList<>(Arrays.asList(pages));
     }
 
+    /**
+     * Moves to the next page.
+     * @param player The player to move the page for.
+     */
     public void nextPage(HumanEntity player) {
         try {
             currentPage++;
@@ -33,5 +37,26 @@ public class PaginatedGUI {
         } catch (IndexOutOfBoundsException e) {
             currentPage--;
         }
+    }
+
+    /**
+     * Returns the previous page of the PaginatedGUI.
+     * @param player The player to open the inventory for.
+     */
+    public void previousPage(HumanEntity player) {
+        try {
+            currentPage--;
+            player.openInventory(pages.get(currentPage).getInventory());
+        } catch (IndexOutOfBoundsException e) {
+            currentPage++;
+        }
+    }
+
+    /**
+     * Adds a page to the PaginatedGUI.
+     * @param page The page to add.
+     */
+    public void addPage(InventoryGUI page) {
+        pages.add(page);
     }
 }
