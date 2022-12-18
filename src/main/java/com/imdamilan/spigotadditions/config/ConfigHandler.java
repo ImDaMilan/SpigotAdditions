6caa15cfec694c0638e11bf6087cf1ac7524bbda
ImDaMilan <imdamilan333@gmail.com>
2022-12-18 15:05:08 +0100

    Config API 1/2

--- src/main/java/com/imdamilan/spigotadditions/config/ConfigHandler.java
+++ src/main/java/com/imdamilan/spigotadditions/config/ConfigHandler.java
@@ -20,11 +20,15 @@ public class ConfigHandler {
             try {
                 ArrayList<T> objects = (ArrayList<T>) clazz.getDeclaredMethod("getObjects").invoke(null);
                 for (Object object : objects) {
+                    String key = "";
+                    for (Field field : clazz.getDeclaredFields())
+                        if (field.isAnnotationPresent(ConfigKey.class)) key = (String) field.get(object);
                     for (Field field : clazz.getDeclaredFields()) {
                         if (field.isAnnotationPresent(Path.class)) {
                             Path path = field.getAnnotation(Path.class);
                             try {
-                                configuration.set(path.value(), field.get(object));
+                                configuration.set(key + path.value(), field.get(object));
+                                configuration.save(file);
                             } catch (IllegalAccessException e) {
                                 e.printStackTrace();
                             }
@@ -33,6 +37,10 @@ public class ConfigHandler {
                 }
             } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException e) {
                 throw new RuntimeException("The class " + clazz.getSimpleName() + " does not have a static getObjects() method that returns an ArrayList of objects!");
+            } catch (ClassCastException e ) {
+                throw new RuntimeException("The class " + clazz.getSimpleName() + " doesn't have a valid @ConfigKey string field!");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
